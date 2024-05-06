601f776bb717a49df8874074d473fd8098eb2496
Jessica Abdoulahi <abdoulahi.jessica@gmail.com>
2024-05-06 19:37:53 +0200

    Récupération des filtres par un script javascript pour cette première étape

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    let filters = document.querySelectorAll("#filters div");
+    
+    for(let filter of filters){
+        filter.addEventListener("click", function(){
+            let tag = this.id;
+
+           
+        });
+    }
+}
